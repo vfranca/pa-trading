8e1b36bf3e2d0b616698222b24009d73a52f5172
Valmir Franca <vfranca3@gmail.com>
2023-01-08 07:23:40 -0300

    Aplicada configuracao de formato de moeda ao comando p2

--- pa_trading/p2.py
+++ pa_trading/p2.py
@@ -2,6 +2,7 @@
 # Copyright 2022 Valmir França da Silva
 # http://github.com/vfranca
 import click
+from pa_trading.conf import formato_moeda
 
 
 # Cria o comando p2
@@ -15,8 +16,8 @@ def p2(final, inicial, recuo):
     perna2 = recuo + perna1
     recuo_em_pontos = abs(final - recuo)
     recuo_percentual = recuo_em_pontos / perna1 * 100
-    click.echo("%.2f" % abs(perna1))
-    click.echo("%.2f" % perna2)
+    click.echo(formato_moeda % abs(perna1))
+    click.echo(formato_moeda % perna2)
     click.echo("%.0f%%" % recuo_percentual)
 
 
